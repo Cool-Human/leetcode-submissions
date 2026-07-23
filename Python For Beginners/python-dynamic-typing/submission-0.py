fd8c6714d677f55d59a4a98a8ba81484dfe630fe
Hari Sharan <115358626+Cool-Human@users.noreply.github.com>
2026-07-23 13:42:25 +0530

    Add: python-dynamic-typing - submission-0

---
+++ Python For Beginners/python-dynamic-typing/submission-0.py
@@ -0,0 +1,14 @@
+variable = int(0)
+print(type(variable))
+
+variable = float(0)
+print(type(variable))
+
+variable = bool(0)
+print(type(variable))
+
+variable = str(0)
+print(type(variable))
+
+variable = [0]
+print(type(variable))
