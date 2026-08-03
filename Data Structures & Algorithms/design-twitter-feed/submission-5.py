21b46eb557ad2846baec8a76c4c86b78cecd564c
Hari Sharan <115358626+Cool-Human@users.noreply.github.com>
2026-08-03 16:26:13 +0530

    Add: design-twitter-feed - submission-5

---
+++ Data Structures & Algorithms/design-twitter-feed/submission-5.py
@@ -0,0 +1,40 @@
+import heapq
+from collections import defaultdict
+
+class Twitter:
+
+    def __init__(self):
+        self.time = 0
+        self.tweets = defaultdict(list)
+        self.follows = defaultdict(set)
+
+    def postTweet(self, userId: int, tweetId: int) -> None:
+        self.tweets[userId].append([self.time, tweetId])
+        self.time += 1
+    
+    def getNewsFeed(self, userId: int) -> List[int]:
+        res = []
+        self.follows[userId].add(userId)
+        heap = []
+
+        for ppl in self.follows[userId]:
+            tweets = self.tweets.get(ppl)
+            if tweets:
+                idx = len(tweets) - 1
+                while idx > -1:
+                    time, tweetId = tweets[idx]
+                    heapq.heappush_max(heap, [time, tweetId])
+                    idx -= 1
+        
+        while heap and len(res) < 10:
+            time, tweetId = heapq.heappop_max(heap)
+            res.append(tweetId)
+        
+        return res
+
+    def follow(self, followerId: int, followeeId: int) -> None:
+        self.follows[followerId].add(followeeId)
+
+    def unfollow(self, followerId: int, followeeId: int) -> None:
+        if followerId in self.follows and followeeId != followerId:
+            self.follows[followerId].discard(followeeId)
