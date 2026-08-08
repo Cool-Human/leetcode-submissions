810a1930f45fda9b74b2104dc73c66766be5edfa
Hari Sharan <115358626+Cool-Human@users.noreply.github.com>
2026-08-08 11:37:26 +0530

    Add: longest-palindrome - submission-1

---
+++ Data Structures & Algorithms/longest-palindrome/submission-1.py
@@ -0,0 +1,17 @@
+class Solution:
+    def longestPalindrome(self, s: str) -> int:
+        odd = False
+        even = []
+        characters = sorted(list(s))
+        
+        i = 0
+        while i < len(s):
+            if i + 1 < len(s) and characters[i] == characters[i + 1]:
+                even.append(characters[i])
+                i += 2
+            else:
+                odd = True
+                i += 1
+
+        
+        return 2 * len(even) + 1 if odd else 2 * len(even)
